3b56c27d7008a3c77d543d102fde6706cb31a182
anishhimself <57608042+anishhimself@users.noreply.github.com>
2022-09-07 15:19:16 +0545

    50% completed

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import About from "./components/About";
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Contact from "./components/Contact";
+import SocialLinks from "./components/SocialLinks";
+
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Contact/>
+      <Footer/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/SocailLinks.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { FaGithub, FaLinkedin, FaFacebook, FaInstagra } from "react-icons/fa";
+import { HiOutlineMail } from "react-icons/hi";
+import { BsFillPersonLinesFill } from "react-icons/bs";
